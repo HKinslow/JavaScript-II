ec6ce13180312bf9649655d48b03ec849735366c
HKinslow <heatherkinslow6@gmail.com>
2019-09-06 13:58:30 -0400

    completed closure.js mandatory challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,18 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+const trouble = () => {
+  //trouble1(); // 1
+  //trouble1(); // 2
+  let troubles = 0;
+  return function() {
+    troubles = troubles + 1;
+    return troubles;
+  }
+}
 
+const trouble1 = trouble();
+console.log(trouble1());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
